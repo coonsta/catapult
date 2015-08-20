6f96305438e262f5042f2490101b8abb765f1586
Dominic Cooney <dominicc@chromium.org>
2015-08-20 11:37:20 +0900

    Note the RAIL event kind along with the event; notes about generating graphs.

---
+++ third_party_time/graphing.r
@@ -0,0 +1,8 @@
+slices <- read.csv("/work/catapult/perf_insights/data-w-kind.csv", sep=",", header=TRUE)
+slices$pctThirdParty <- slices$thirdParty / slices$duration
+slices$pctFirstParty <- slices$firstParty / slices$duration
+slices$pctUnaccounted <- slices$unaccounted / slices$duration
+plot(y=slices$pctThirdParty, x=log10(slices$duration), col=slices$kind)
+legend('topleft', legend=levels(slices$kind), col=1:length(slices$kind),pch=1)
+abline(v=log10(50), col="purple")
+abline(v=log10(16), col="orange")
--- third_party_time/log2csv.awk
+++ third_party_time/log2csv.awk
@@ -1,7 +1,8 @@
 #!/usr/bin/awk -f
 # This relies on hash iteration order: thirdParty, firstParty, unaccounted
-BEGIN { print "duration,thirdParty,firstParty,unaccounted" }
+BEGIN { print "duration,thirdParty,firstParty,unaccounted,kind" }
 /duration/ { printf "%s", $2 }
 /thirdParty/ { printf "%s", $2 }
 /firstParty/ { printf "%s", $2 }
-/unaccounted/ { print $2 }
+/unaccounted/ { printf "%s,", $2 }
+/kind/ { print $2 }
